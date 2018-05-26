8c272b9986ced27718106ea019d47d49a209f8b0
Bassel Ezzedine <bassel-maro@hotmail.com>
2018-05-26 16:12:19 +0200

    First commit

---
+++ WeatherAssist/WeatherAssist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherAssist
+//
+//  Created by Bassel Ezzeddine on 26/05/2018.
+//  Copyright © 2018 Bassel Ezzeddine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
