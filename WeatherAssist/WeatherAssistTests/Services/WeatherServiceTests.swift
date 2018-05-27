c8b38ddf02b5289cb7304255eb3d123069c56f1f
Bassel Ezzeddine <bassel-maro@hotmail.com>
2018-05-27 13:58:58 +0200

    Installed GCDWebServer pod

---
+++ WeatherAssist/WeatherAssistTests/Services/WeatherServiceTests.swift
@@ -0,0 +1,35 @@
+//
+//  WeatherServiceTests.swift
+//  WeatherAssistTests
+//
+//  Created by Bassel Ezzeddine on 27/05/2018.
+//  Copyright © 2018 Bassel Ezzeddine. All rights reserved.
+//
+
+import XCTest
+
+class WeatherServiceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
