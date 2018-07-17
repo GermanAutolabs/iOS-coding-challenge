25b00b8b41bd0ad3c63b5bee9f63d5bddab6cbc9
Antonio de Mingo <tonidemingo@gmail.com>
2018-07-17 09:52:51 +0200

    Model - Weather data request

--- iOS coding challenge for German Autolabs/iOS coding challenge for German Autolabs/Model/Weather.swift
+++ iOS coding challenge for German Autolabs/iOS coding challenge for German Autolabs/Model/Weather.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 struct Weather {
     let summary:String
@@ -26,6 +27,38 @@ struct Weather {
         self.summary = summary
         self.icon = icon
         self.temperature = temperature
+    }
+    
+    static let basePath = "https://api.darksky.net/forecast/d2bba1adab38d52cfaa298aee4fd2634/"
+    
+    static func forecast (withLocation location:CLLocationCoordinate2D, completion: @escaping ([Weather]?) -> ()) {
+        
+        let url = basePath + "\(location.latitude),\(location.longitude)"
+        let request = URLRequest(url: URL(string: url)!)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data:Data?, response:URLResponse?, error:Error?) in
+            
+            var forecast: [Weather] = []
+            
+            if let data = data {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
+                        
+                        if let currently = json["currently"] as? [String:Any] {
+                            
+                            if let weatherObject = try? Weather(json: currently) {
+                                
+                                forecast.append(weatherObject)
+                            }
+                        }
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                }
+                completion(forecast)
+            }
+        }
+        task.resume()
     }
     
     
