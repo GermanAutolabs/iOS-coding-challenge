80985ae2f8271699d08122daf6f3896b0c5eecf2
Antonio de Mingo <tonidemingo@gmail.com>
2018-07-17 08:20:21 +0200

    Project creation

---
+++ iOS coding challenge for German Autolabs/iOS coding challenge for German AutolabsTests/iOS_coding_challenge_for_German_AutolabsTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_coding_challenge_for_German_AutolabsTests.swift
+//  iOS coding challenge for German AutolabsTests
+//
+//  Created by Antonio De Mingo Navarro on 17/07/2018.
+//  Copyright © 2018 Antonio De Mingo Navarro. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_coding_challenge_for_German_Autolabs
+
+class iOS_coding_challenge_for_German_AutolabsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
