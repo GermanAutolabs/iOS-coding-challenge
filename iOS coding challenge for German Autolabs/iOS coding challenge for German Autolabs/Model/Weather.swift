bf29cb64fd0b220590382eb2897cae16a840b9ce
Antonio de Mingo <tonidemingo@gmail.com>
2018-07-17 09:08:47 +0200

    Model - Weather

--- iOS coding challenge for German Autolabs/iOS coding challenge for German Autolabs/Model/Weather.swift
+++ iOS coding challenge for German Autolabs/iOS coding challenge for German Autolabs/Model/Weather.swift
@@ -12,4 +12,23 @@ struct Weather {
     let summary:String
     let icon:String
     let temperature:Double
+    
+    enum SerializationError:Error {
+        case missing(String)
+        case invalid(String, Any)
+    }
+    
+    init(json:[String:Any]) throws {
+        guard let summary = json["summary"] as? String else { throw SerializationError.missing("summary is missing") }
+        guard let icon = json["icon"] as? String else { throw SerializationError.missing("icon is missing") }
+        guard let temperature = json["temperature"] as? Double else { throw SerializationError.missing("temp is missing") }
+        
+        self.summary = summary
+        self.icon = icon
+        self.temperature = temperature
+    }
+    
+    
+    
+    
 }
