f527cf55c84bee8e01ad400f79b540f99e07e708
Antonio de Mingo <tonidemingo@gmail.com>
2018-07-17 08:32:05 +0200

    Project structure

---
+++ iOS coding challenge for German Autolabs/iOS coding challenge for German Autolabs/Controllers/SpeechRecognizerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SpeechRecognizerViewController.swift
+//  iOS coding challenge for German Autolabs
+//
+//  Created by Antonio De Mingo Navarro on 17/07/2018.
+//  Copyright © 2018 Antonio De Mingo Navarro. All rights reserved.
+//
+
+import UIKit
+
+class SpeechRecognizerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+
+}
---
+++ iOS coding challenge for German Autolabs/iOS coding challenge for German Autolabs/Model/City.swift
@@ -0,0 +1,9 @@
+//
+//  City.swift
+//  iOS coding challenge for German Autolabs
+//
+//  Created by Antonio De Mingo Navarro on 17/07/2018.
+//  Copyright © 2018 Antonio De Mingo Navarro. All rights reserved.
+//
+
+import Foundation
