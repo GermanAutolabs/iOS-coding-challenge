f1128b82385db40a3c3d3430b99e5f594a2da16d
Etienne Negro <etienne.wojahn@me.com>
2018-10-02 11:46:10 +0200

    Add fetching weather conditions from worldweatheronline
    - Create the ConnectionManager to get the data from worldweatheronline
    - Create a struct to represent the data that should be displayed

---
+++ WeatherVoice/manager/ConnectionManager.swift
@@ -0,0 +1,66 @@
+//
+//  ConnectionManager.swift
+//  WeatherVoice
+//
+//  Created by Ed Negro on 02.10.18.
+//  Copyright © 2018 Etienne Negro. All rights reserved.
+//
+
+import Alamofire
+
+protocol Connection {
+    func getWeatherInfo(location: String, date: String, completion: @escaping (Weather?) -> Void)
+}
+
+class ConnectionManager: Connection {
+    let baseUrl = "https://api.worldweatheronline.com"
+    let apiKey = "b1928ca6bdec4310953195111180110"
+
+    func getWeatherInfo(location: String, date: String, completion: @escaping (Weather?) -> Void) {
+
+        let params = ["format": "json",
+            "q": "48.834,2.394", //"berlin",
+            "key": apiKey,
+            "date": "today"]
+
+        Alamofire.request("\(baseUrl)/premium/v1/weather.ashx", parameters: params).responseJSON { response in
+            response.result.ifSuccess {
+                if let current: Dictionary<String, Any> = self.extractCurrent(json: response.result.value as! Dictionary<String, Any>) {
+                    completion(self.createWeather(_from: current))
+                }
+            }
+
+            response.result.ifFailure {
+                completion(nil)
+                debugPrint("failed")
+            }
+        }
+    }
+
+    func extractCurrent(json: Dictionary<String, Any>) -> Dictionary<String, Any>? {
+        if let data: Dictionary<String, Any> = json["data"] as? Dictionary<String, Any> {
+            if let current = data["current_condition"] as? [[String: Any]] {
+                return current[0]
+            }
+        }
+        return nil
+    }
+
+    func createWeather (_from data: Dictionary<String, Any>) -> Weather {
+        var weather = Weather()
+        weather.tempC = Int(data["temp_C"] as! String) ?? 0
+        weather.tempFeelsC = Int(data["FeelsLikeC"] as! String) ?? 0
+        weather.humidity = Int(data["humidity"] as! String) ?? 0
+        weather.windspeedKmph = Int(data["windspeedKmph"] as! String) ?? 0
+
+        if let desc = data["weatherDesc"] as? [[String: String]] {
+            weather.desc = desc[0]["value"] ?? ""
+        }
+
+        if let icon = data["weatherIconUrl"] as? [[String: String]] {
+            weather.icon = icon[0]["value"] ?? ""
+        }
+
+        return weather
+    }
+}
---
+++ WeatherVoice/model/Weather.swift
@@ -0,0 +1,15 @@
+//
+//  Weather.swift
+//  WeatherVoice
+//
+//  Created by Ed Negro on 02.10.18.
+//  Copyright © 2018 Etienne Negro. All rights reserved.
+//
+struct Weather {
+    var tempC = 0
+    var tempFeelsC = 0
+    var humidity = 0
+    var desc = ""
+    var icon = ""
+    var windspeedKmph = 0
+}
