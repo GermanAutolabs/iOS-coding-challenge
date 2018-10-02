febe85bdb87290a40babb7c647f44c894a186eb3
Etienne Negro <etienne.wojahn@me.com>
2018-10-02 11:48:17 +0200

    Create test to verify serialization
    Tests to verify that the serialization process is performed correctly and the correct initialized struct is returned

--- WeatherVoiceTests/WeatherVoiceTests.swift
+++ WeatherVoiceTests/WeatherVoiceTests.swift
@@ -11,24 +11,37 @@ import XCTest
 
 class WeatherVoiceTests: XCTestCase {
 
+    var connection = ConnectionManager()
+    var reponseJson: Dictionary<String, AnyObject>!
+
+    // Serialize the weather.json to have stable data to check the model building process
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        if let path = Bundle.main.path(forResource: "weather", ofType: "json") {
+            let data = try! Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+            let jsonResult = try! JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+            if let jsonResult = jsonResult as? Dictionary<String, AnyObject> {
+                self.reponseJson = jsonResult
+            }
         }
     }
 
+    // Check if the current condition is gathered correctly from the json
+    func testGetCurrentCondition() {
+        let current = connection.extractCurrent(json: reponseJson)
+        assert(current != nil)
+        assert(current?.keys.count == 17)
+    }
+
+    // Check if the created model contains the right values
+    func testModelCreation() {
+        if let current = connection.extractCurrent(json: reponseJson) {
+            let model = connection.createWeather(_from: current)
+            assert(model.tempC == 7)
+            assert(model.tempFeelsC == 4)
+            assert(model.humidity == 87)
+            assert(model.windspeedKmph == 20)
+            assert(model.desc == "Partly cloudy")
+            assert(model.icon.count > 0)
+        }
+    }
 }
