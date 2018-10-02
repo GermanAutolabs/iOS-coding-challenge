3cd289177229b9aba40555a006ee9176ed7ef27c
Etienne Negro <etienne.wojahn@me.com>
2018-10-02 12:50:45 +0200

    Create the view and fill it with data
    - Fetch data after pressing the microphone button
    - Show labels to present the data
    - Fill the labels with the data from the worldweatheronline api

--- WeatherVoice/vc/ViewController.swift
+++ WeatherVoice/vc/ViewController.swift
@@ -10,18 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var weatherDescription: UILabel!
+    @IBOutlet weak var weatherTemperatur: UILabel!
+
+    @IBOutlet weak var weatherTempFeel: UILabel!
+    @IBOutlet weak var weatherWindSpeed: UILabel!
+    @IBOutlet weak var weatherHumidity: UILabel!
+
+    @IBOutlet weak var askLabel: UILabel!
+
     let connectionManager: Connection = ConnectionManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        connectionManager.getWeatherInfo(location: "Berlin", date: "today") { (weather) in
-            debugPrint(weather)
-        }
-
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func micButtoPressed(_ sender: Any) {
+        askLabel.isHidden = true
+
+        connectionManager.getWeatherInfo(location: "Berlin", date: "today") { (weather) in
+
+            if weather != nil {
+                self.weatherTemperatur.isHidden = false
+                self.weatherTemperatur.text = "\(String(describing: weather!.tempC)) °C"
+
+                self.weatherDescription.isHidden = false
+                self.weatherDescription.text = weather!.desc
+
+                self.weatherTempFeel.isHidden = false
+                self.weatherTempFeel.text = "\(String(describing: weather!.tempFeelsC)) °C"
+
+                self.weatherWindSpeed.isHidden = false
+                self.weatherWindSpeed.text = "\(String(describing: weather!.windspeedKmph)) kmph"
+
+                self.weatherHumidity.isHidden = false
+                self.weatherHumidity.text = "\(String(describing: weather!.humidity)) %"
+            }
+
+        }
+
+    }
 
 }
 
