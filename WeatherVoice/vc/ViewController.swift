7bec57d51be350dd130bc0766ce2972cec38e86e
Etienne Negro <etienne.wojahn@me.com>
2018-10-02 08:42:21 +0200

    inital commit

---
+++ WeatherVoice/vc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherVoice
+//
+//  Created by Ed Negro on 02.10.18.
+//  Copyright © 2018 Etienne Negro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
